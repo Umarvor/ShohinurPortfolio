efdd795cd7d6f1e9c9eb23be5ad7025c18a72989
Shohinur <shohinurshahriyorov@gmail.com>
2024-09-30 19:29:38 +0500

    f

--- src/Malumot/Ozim.jsx
+++ src/Malumot/Ozim.jsx
@@ -8,7 +8,7 @@ function Profile() {
       <div className="profile-info">
         <h1>O'zim haqimda!</h1>
         <p>
-          Salom mening ismim Shohinur familyam Shahriyorov 26-dekabr 2008-yilda tug'ulganman. Yashash manzilim Qashqadaryo viloyati kitob tumani Varganza MFY. Hozirda tumandagi 96-maktabning metematika-fizika fanlarini chuqurlashtirib o'tadigan sinfga o'qiyman. Yoshligimdan dasturlash va kiberxafsizkikka qiziqqanim sababli  IT yo'nalishida  o'qimoqchiman. O'tgan 2023 yil yoz oyida kitob tumandagi IQTIDOR IT maktabigqa front-end kursiga o'qishga bordim va u yerni muvofiqiyatli tamomlab "Junior dasturchi sertifikatiga ega bo'dim" hozida ber nechta saytlar dasturchisiman.Kelajakdagi maqsadim Ozbekiston respublikasini rivojlanishiga hissa qo'shish va yetuk dasturchi bo'lish va eng asosisi "Shoh Group " IT kompaniyasiga asos solish!
+          Salom mening ismim Shohinur familyam Shahriyorov 26-dekabr 2008-yilda tug'ulganman. Yashash manzilim Qashqadaryo viloyati kitob tumani Varganza MFY. Hozirda tumandagi 96-maktabning metematika-fizika fanlarini chuqurlashtirib o'tadigan sinfga o'qiyman. Yoshligimdan dasturlash va kiberxafsizkikka qiziqqanim sababli  IT yo'nalishida  o'qimoqchiman. O'tgan 2023 yil yoz oyida kitob tumandagi IQTIDOR IT maktabigqa front-end kursiga o'qishga bordim va u yerni muvofiqiyatli tamomlab "Junior dasturchi sertifikatiga ega bo'dim" hozida ber nechta saytlar dasturchisiman.Kelajakdagi maqsadim Ozbekiston respublikasini rivojlanishiga hissa qo'shish va yetuk dasturchi bo'lish va eng asosisi "Shoh Group " IT kompaniyasiga asos solishdir!
         </p>
       </div>
       <div className="profile-image">
